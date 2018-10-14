fd91d985de6cc2feef61402664012c91e5d97bf1
bleedyao <logcat001@gmai.com>
2018-10-15 07:59:23 +0800

    finish two sum

---
+++ src/round01/Code01_Two_Sum.java
@@ -0,0 +1,26 @@
+package round01;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Code01_Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int findNum = target - nums[i];
+            if (map.containsKey(findNum)) {
+                return new int[]{map.get(findNum), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] caseArr = {2, 7, 9, 11, 15};
+        int[] result = new Code01_Two_Sum().twoSum(caseArr, 26);
+        System.out.println(Arrays.toString(result));
+    }
+}
